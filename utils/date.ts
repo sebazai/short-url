aab7723751c4b4c0fe2a0853555fc0fd3f8d4670
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2022-03-05 23:52:45 +0100

    Add leading zeros to date for sorting

--- utils/date.ts
+++ utils/date.ts
@@ -2,5 +2,5 @@
 export const getUTCDateWithoutTime = (): string => {
     const today = new Date()
     // Are you kidding me getUTCMonth...
-    return `${today.getUTCFullYear()}-${today.getUTCMonth() + 1}-${today.getUTCDate()}`
+    return `${today.getUTCFullYear()}-${("0" + (today.getUTCMonth() + 1)).slice(-2)}-${("0" + today.getUTCDate()).slice(-2)}`
 }
