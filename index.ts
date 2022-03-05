99fbd0c62c94eb98fcbf9211677ea7f9524f8f24
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2022-03-05 22:53:47 +0100

    Move to server.ts

--- index.ts
+++ index.ts
@@ -6,12 +6,4 @@ import path from "path"
 const PORT = Number(process.env.PORT || c.get("PORT") || 8080)
 app.listen(PORT, () => {
   console.log(`Application started on port ${PORT}`)
-})
-
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'frontend/build')))
-
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
-})
+}) 
--- server.ts
+++ server.ts
@@ -7,6 +7,7 @@ import db from "./config/db"
 import urlRouter from "./routes/url"
 import defaultRouter from "./routes/index"
 import statsRouter from "./routes/stats"
+import path from "path"
 
 // Connect to MongoDB
 db()
@@ -31,6 +32,15 @@ if (process.env.NODE_ENV === "production" || c.get("NODE_ENV") === "production")
 app.use("/", defaultRouter)
 app.use("/api/url", urlRouter)
 app.use("/api/stats", statsRouter)
+
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'frontend/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+})
 
 app.use((err: Error, req: express.Request, res: express.Response) => {
     return res.status(500).json({
